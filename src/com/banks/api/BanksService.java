3c6e9d906420ce71bdbe33fa22007c0380298e75
wellysonvie <wellysonvie@gmail.com>
2020-11-28 18:27:58 -0300

    feat: retornando codigo de status e mensagens

--- src/com/banks/api/BanksService.java
+++ src/com/banks/api/BanksService.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.banks.dao.BankDAO;
 import com.banks.entities.*;;
@@ -26,24 +27,32 @@ public class BanksService {
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<Bank> listAllBanks(){
+	public Response listAllBanks(){
 		
 		ArrayList<Bank> banks = null;
 		banks = bankDAO.getAll();
 		
-		return banks;
+		if(banks.size() == 0) {
+			return Response.status(204).entity("Nenhum banco cadastrado!").build();
+		}
+		
+		return Response.status(200).entity(banks).build();
 	}
 	
 	@GET
 	@Path("/{code}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Bank getBankByCompensationCode(@PathParam("code") int code){
+	public Response getBankByCompensationCode(@PathParam("code") int code){
 		
 		Bank bank = null;
 		bank = bankDAO.getByCompensationCode(code);
 		
-		return bank;
+		if(bank == null) {
+			return Response.status(404).entity("Nenhum banco encontrado!").build();
+		}
+		
+		return Response.status(200).entity(bank).build();
 	}
 
 }
